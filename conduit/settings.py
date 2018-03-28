308063507b8482404e76d4341cdf99d4a38736c1
Steve Dower <steve.dower@microsoft.com>
2018-03-28 11:38:12 -0700

    Update settings.py

--- conduit/settings.py
+++ conduit/settings.py
@@ -23,9 +23,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '2^f+3@v7$v1f8yt0!s)3-1t$)tlp+xm17=*g))_xoi&&9m#2a&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'rwa-be.azurewebsites.net',
+    'rwa-fe.azurewebsites.net',
+]
 
 
 # Application definition
