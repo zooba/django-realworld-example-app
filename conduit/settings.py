f3a147cdb8de57e9b79becf872818c9d5478c283
Steve Dower <steve.dower@microsoft.com>
2018-03-28 12:05:16 -0700

    Add STATIC_ROOT value

--- conduit/settings.py
+++ conduit/settings.py
@@ -131,6 +131,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 CORS_ORIGIN_WHITELIST = (
     '0.0.0.0:4000',
